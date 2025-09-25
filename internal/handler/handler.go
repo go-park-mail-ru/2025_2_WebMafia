b6f9b2764a73af34cd79886742a23f9f56e4bda7
LemeshkinaVeronika <lemeshkinava@student.bmstu.ru>
2025-09-25 19:57:33 +0300

    [Task 4] handler fix

--- internal/handler/handler.go
+++ internal/handler/handler.go
@@ -1,6 +1,7 @@
 package handler
 
 import (
+	"spotify/internal/mock"
 	"spotify/internal/model"
 	"sync"
 )
@@ -8,11 +9,13 @@ import (
 type Handlers struct {
 	users []model.User
 	mu    *sync.RWMutex
+	store *mock_store.MockStore
 }
 
 func NewHandler() *Handlers {
 	return &Handlers{
 		users: make([]model.User, 0),
 		mu:    &sync.RWMutex{},
+		store: mock_store.NewMockStore(),
 	}
 }

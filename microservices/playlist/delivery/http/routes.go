77898b7aee409dd36a68bd79f3fb9db10ce7f14d
LemeshkinaVeronika <lemeshkinava@student.bmstu.ru>
2025-11-24 21:38:37 +0300

    [TASK 31] fix routes

--- microservices/playlist/delivery/http/routes.go
+++ microservices/playlist/delivery/http/routes.go
@@ -8,7 +8,7 @@ import (
 
 func (h *Handler) RegisterRoutes(public, protected *mux.Router) {
 	protected.HandleFunc("/playlists/favorite", h.GetFavoritePlaylist).Methods(http.MethodGet, http.MethodOptions)
-	protected.HandleFunc("/playlists/favorite/add-track", h.AddTrackToFavorite).Methods(http.MethodPost)
+	protected.HandleFunc("/playlists/favorite/add-track", h.AddTrackToFavorite).Methods(http.MethodPost, http.MethodOptions)
 
 	public.HandleFunc("/playlists/{id}", h.GetPlaylistByID).Methods(http.MethodGet, http.MethodOptions)
 	public.HandleFunc("/users/{userId}/playlists", h.GetAllPlaylistsByUserID).Methods(http.MethodGet, http.MethodOptions)
@@ -17,8 +17,8 @@ func (h *Handler) RegisterRoutes(public, protected *mux.Router) {
 	protected.HandleFunc("/playlists/{id}", h.UpdatePlaylist).Methods(http.MethodPut, http.MethodOptions)
 	protected.HandleFunc("/playlists/{id}", h.DeletePlaylist).Methods(http.MethodDelete, http.MethodOptions)
 
-	protected.HandleFunc("/playlists/{id}/avatar", h.UploadPlaylistAvatar).Methods(http.MethodPost)
-	protected.HandleFunc("/playlists/{id}/avatar", h.DeletePlaylistAvatar).Methods(http.MethodDelete)
+	protected.HandleFunc("/playlists/{id}/avatar", h.UploadPlaylistAvatar).Methods(http.MethodPost, http.MethodOptions)
+	protected.HandleFunc("/playlists/{id}/avatar", h.DeletePlaylistAvatar).Methods(http.MethodDelete, http.MethodOptions)
 
 	protected.HandleFunc("/playlists/my", h.GetMyPlaylists).Methods(http.MethodGet, http.MethodOptions)
 
